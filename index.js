e566502cb39503b0c0d325d15013d5fee3eb4f2e
Amartya Sarkar <sarkaramartya0@gmail.com>
2020-11-22 17:34:03 +0530

    Add cool face API

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+var mongoose=require("mongoose");
 var cors = require('cors');
+
 const app = express();
 const port = process.env.PORT || "8000";
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static("public"));
 
 app.use(cors());
+require("./Schema/DeviceType");
+require("./Schema/Classification");
+require("./Schema/Model");
+require("./Schema/Color");
+require("./Schema/issue");
+
+const mongoURI="mongodb+srv://amartyasarkar0001:amartyasarkar1@cluster0.fp2ed.mongodb.net/DeviceRepair?retryWrites=true&w=majority"
+
+mongoose.connect(mongoURI,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useFindAndModify: false 
+})
+mongoose.connection.on("connected",()=>{
+    console.log("Yeah !!!!,I have Successfully connected with Mongodb Atlas")
+})
+mongoose.connection.on("error",(err)=>{
+    console.log("Error",err)
+})
 
 
 var nodemailer = require('nodemailer');
@@ -16,13 +38,10 @@ var transporter = nodemailer.createTransport({    //fror link your email and nod
   service: 'gmail',
   secure: true,
   auth: {
-    user: 'amartyasarkark8n0cx06w349338@gmail.com', //Your Email Address
-    pass: '@<?#Vivek&*ma349338$>%'      //Your Password
+    user: 'Your Email', //Your Email Address
+    pass: 'Your Password'      //Your Password
   }
-  // auth: {
-  //   user: 'amartyasarkar4@gmail.com',
-  //   pass: '@<?#Vivek&*ma4$>%'
-  // }
+  
 });
 
 app.post("/sendmail", (req, res) => {
@@ -81,6 +100,12 @@ app.post("/sendmail", (req, res) => {
     }
   });
 })
+//amartyasarkar0001
+//amartyasarkar1
+//mongodb+srv://amartyasarkar0001:<password>@cluster0.fp2ed.mongodb.net/<dbname>?retryWrites=true&w=majority
+app.use(express.json());
+app.use(require("./routes/handling"));
+
 
 app.listen(port, () => {
   console.log(`Listening to requests on http://localhost:${port}`);
